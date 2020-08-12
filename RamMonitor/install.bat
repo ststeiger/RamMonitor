44049133ec23065831f52f0400356168a2e2299b
ststeiger <quandary82@hailmail.net>
2020-08-12 11:26:16 +0200

    Fix install.bat

---
+++ RamMonitor/install.bat
@@ -0,0 +1,25 @@
+REM https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/sc-create
+REM https://docs.oracle.com/cd/E29805_01/server.230/es_admin/src/radm_service_sc_syntax.html
+REM installutil <yourproject>.exe
+REM installutil /u <yourproject>.exe
+REM sc create MyBinary binpath="c:\windows\system32\NewServ.exe" type=share start=auto depend= "+TDI NetBIOS"
+REM sc create svnserve 
+REM    binpath= "\"C:\Program Files\CollabNet Subversion Server\svnserve.exe\" --service -r \"C:\my repositories\"  "
+REM    displayname= "Subversion Server" depend= Tcpip start= auto 
+
+
+SET service_path=%~dp0RamMonitor.exe
+sc create CorRamMonitor DisplayName="COR RamMonitor" binpath="%service_path%" start=auto
+sc description "CorRamMonitor" "Monitors RAM usage and kills chrome processes"
+sc config CorRamMonitor type=interact type=own
+
+REM https://stackoverflow.com/questions/15085856/using-sc-to-install-a-windows-service-and-then-set-recovery-properties
+REM https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-r2-and-2012/cc742019(v%3Dws.11)
+REM https://stackoverflow.com/questions/22872510/how-to-use-sc-to-install-a-service-and-specify-no-action-for-subsequent-failu
+sc failure CorRamMonitor reset=86400 actions=restart/5000/restart/5000/restart/5000
+
+sc start CorRamMonitor 
+
+
+
+REM sc.exe delete "CorRamMonitor"
